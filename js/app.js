374904726b05a80348750e6d7db64dd8363d1df8
Brady Davenport <brady.davenport@pm.me>
2022-04-05 13:15:00 -0600

    initial coding commit

---
+++ js/app.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let siteVisitor = prompt('Hey! What\'s your name?');
+alert(`Welcome ${siteVisitor}, let's play a guessing game. Please answer Yes or No`);
+
+let questionOne = prompt().toLowerCase;
+if (questionOne === 'yes' || questionOne === 'y') {
+  alert()
+} else if (questionOne === 'no' || questionOne === 'n') {
+  alert();
+} else {
+  alert('Please answer with Yes or No');
+}
+
+let questionTwo = prompt();
+if (questionTwo === || questionTwo === ) {
+  alert()
+} else if () {
+  alert();
+}
+
+let questionThree = prompt();
+if (questionThree === || questionThree === ) {
+  alert()
+} else if () {
+  alert();
+}
+
+let questionFour = prompt();
+if (questionFour === || questionFour === ) {
+  alert()
+} else if () {
+  alert();
+}
+
+let questionFive = prompt();
+if (questionFive === || questionFive === ) {
+  alert()
+} else if () {
+  alert();
+}
