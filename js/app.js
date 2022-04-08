5df022a77a5244ea6ae599f0082d56755bc75e5b
Brady Davenport <brady.davenport@pm.me>
2022-04-07 19:00:00 -0600

    fixing lab-03 script

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ function q1(){
     alert('Actually, I really was born and raised in Pueblo, Colorado.');
   } else {
   //console.log('Please answer with Yes or No');
-    alert('Please answer with Yes or No');
+    prompt('Please answer Yes or No').toLowerCase();
   }
 }
 q1();
@@ -90,50 +90,63 @@ function q6(){
     return Math.floor(Math.random() * max);
   }
 
-  let questionSix = prompt('Guess my favorite number');
-
   let randomNumber = getRandomNumber(10);
   console.log(randomNumber);
+
   let numberOfAttempts = 0;
 
-  while (questionSix !== randomNumber && numberOfAttempts < 5) {
-    if (questionSix < randomNumber) {
-      questionSix = prompt('Too low, guess again.');
+  for (let i = 0; i < 4; i++) {
+    let questionSix = parseInt(prompt('Try to guess the number I am thinking of.'));
+    if (questionSix === randomNumber) {
+      numberOfAttempts++;
+      if (numberOfAttempts < 2) {
+        alert(`Nice guess! It is ${randomNumber}! It only took you ${numberOfAttempts} try to guess correctly.`);
+      } else {
+        alert(`Nice guess! It is ${randomNumber}! It took you ${numberOfAttempts} tries to guess correctly.`);
+      }
+      numberOfCorrectAnswers++;
+      break;
+    } else if (questionSix > randomNumber && i < 3) {
+      alert('Too high, guess again');
+      numberOfAttempts++;
+    } else if (questionSix < randomNumber && i < 3) {
+      alert('Too low, guess again');
+      numberOfAttempts++;
     }
-    if (questionSix > randomNumber) {
-      questionSix = prompt('Too high, guess again');
+    if (i === 3 && questionSix !== randomNumber) {
+      alert(`Sorry, you've run out of guess attempts, the correct answer is ${randomNumber}`);
     }
-    numberOfAttempts ++;
-  }
-
-  if (questionSix === randomNumber) {
-    alert(`Nice guess! It is ${randomNumber}! It took you ${numberOfAttempts} to guess correctly.`);
-    numberOfCorrectAnswers++;
-  } else {
-    alert(`Sorry, you have run out of guesses! My favorite number is ${randomNumber}.`);
   }
 }
 q6();
 
 function q7(){
-  let myFavoriteFoods = ['Steak', 'Pizza', 'Ice Cream', 'Korean BBQ', 'Orange Chicken', 'Gnocchi', 'Cheesecake', 'Anything with cheese'];
+  let myFavoriteFoods = ['steak', 'pizza', 'ice cream', 'korean bbq', 'orange chicken', 'gnocchi', 'cheesecake', 'anything with cheese'];
 
-  let questionSeven = prompt('Try to guess a favorite food of mine');
+  let questionSeven = prompt('Try to guess a favorite food of mine').toLowerCase();
 
-  let favoriteFoodGuesses = 0;
-  for (let i = 0; i < myFavoriteFoods.length; i++) {
-    let oneFavoriteFood = myFavoriteFoods[i];
-    if (questionSeven !== oneFavoriteFood && favoriteFoodGuesses < 7) {
-      prompt('Nope, guess again.');
-      favoriteFoodGuesses++;
-    } else if (questionSeven === oneFavoriteFood) {
-      alert(`Correct! ${oneFavoriteFood} is one of my favorite foods!`);
-      numberOfCorrectAnswers++;
-      break;
-    } else {
-      alert(`Sorry, you've run out of guesses. Here is a list of some of my favorite foods: ${myFavoriteFoods}.`);
+  for (let i = 0; i < 6; i++) {
+    if (i > 0) {
+      questionSeven = prompt('Sorry, try again').toLowerCase();
+    }
+    for (let j = 0; j < myFavoriteFoods.length; j++) {
+      console.log('i: ', i, 'j: ', j);
+      if (questionSeven === myFavoriteFoods[j]) {
+        alert(`Yes! I love ${myFavoriteFoods[j]}!`);
+        i = 6;
+        numberOfCorrectAnswers++;
+        break;
+      } else if (questionSeven !== myFavoriteFoods[j] && i < 5) {
+        continue;
+      }
+    }
+    if (i === 5) {
+      alert('Sorry, you\'ve run out of guesses!');
     }
   }
+
+  alert(`Here is a full list of some of my favorite foods: ${myFavoriteFoods}`);
+
 }
 q7();
 
