925b7b4081501e5866e0eb740c3a132d2553ef96
Brady Davenport <brady.davenport@pm.me>
2022-04-08 17:07:23 -0600

    polished up for deployment, removed console logs, comments

--- js/app.js
+++ js/app.js
@@ -8,14 +8,11 @@ alert(`Welcome ${siteVisitor}, let's play a guessing game. Please answer Yes or 
 function q1(){
   let questionOne = prompt('Was I born where I currently live, in Pueblo, Colorado?').toLowerCase();
   if (questionOne === 'yes' || questionOne === 'y') {
-  //console.log('That\'s correct! I was born and raised in Pueblo, Colorado');
     alert('That\'s correct! I was born and raised in Pueblo, Colorado');
     numberOfCorrectAnswers++;
   } else if (questionOne === 'no' || questionOne === 'n') {
-  //console.log('Actually, I really was born and raised in Pueblo, Colorado.');
     alert('Actually, I really was born and raised in Pueblo, Colorado.');
   } else {
-  //console.log('Please answer with Yes or No');
     prompt('Please answer Yes or No').toLowerCase();
   }
 }
@@ -24,14 +21,11 @@ q1();
 function q2(){
   let questionTwo = prompt('Do I have a pet bulldog?').toLowerCase();
   if (questionTwo === 'yes' || questionTwo === 'y') {
-    //console.log('Well I do have a dog, but she\'s a German Shepherd!');
     alert('Well I do have a dog, but she\'s a German Shepherd!');
   } else if (questionTwo === 'no' || questionTwo === 'n') {
-    //console.log('That\'s correct! I do not have a bulldog, rather, I have a German Shepherd!');
     alert('That\'s correct! I do not have a bulldog, rather, I have a German Shepherd!');
     numberOfCorrectAnswers++;
   } else {
-    //console.log('Please answer with Yes or No');
     alert('Please answer with Yes or No');
   }
 }
@@ -40,14 +34,11 @@ q2();
 function q3(){
   let questionThree = prompt('Do I have any siblings?').toLowerCase();
   if (questionThree === 'yes'|| questionThree === 'y') {
-    //console.log('Yeah! I have 1 older sister, who is a nurse practitioner!');
     alert('Yeah! I have 1 older sister, who is a nurse practitioner!');
     numberOfCorrectAnswers++;
   } else if (questionThree === 'no' || questionThree === 'n') {
-    //console.log('Actually I do! I have 1 older sister, who is a nurse practitioner!');
     alert('Actually I do! I have 1 older sister, who is a nurse practitioner!');
   } else {
-    //console.log('Please answer with Yes or No');
     alert('Please answer with Yes or No');
   }
 }
@@ -56,14 +47,11 @@ q3();
 function q4(){
   let questionFour = prompt('Is it true that I have never served in the military?').toLowerCase();
   if (questionFour === 'yes' || questionFour === 'y') {
-    //console.log('Actually, it is true. I served in the U.S. Marine Corps from 2012 to 2016.');
     alert('Actually, it is true. I served in the U.S. Marine Corps from 2012 to 2016.');
   } else if (questionFour === 'no' || questionFour === 'n') {
-    //console.log('Correct answer! I served in the U.S. Marine Corps from 2012 to 2016.');
     alert('Correct answer! I served in the U.S. Marine Corps from 2012 to 2016.');
     numberOfCorrectAnswers++;
   } else {
-    //console.log('Please answer with Yes or No');
     alert('Please answer with Yes or No');
   }
 }
@@ -72,14 +60,11 @@ q4();
 function q5(){
   let questionFive = prompt('Do I prefer pineapple on my pizza?').toLowerCase();
   if (questionFive === 'yes' || questionFive === 'y') {
-    //console.log('Ewww! No! I most certainly do not like pineapple on my pizza.');
     alert('Ewww! No! I most certainly do not like pineapple on my pizza.');
   } else if (questionFive === 'no' || questionFive === 'n') {
-    //console.log('You, my friend, are correct! I do NOT like pineapple on my pizza!');
     alert('You, my friend, are correct! I do NOT like pineapple on my pizza!');
     numberOfCorrectAnswers++;
   } else {
-    //console.log('Please answer with Yes or No');
     alert('Please answer with Yes or No');
   }
 }
